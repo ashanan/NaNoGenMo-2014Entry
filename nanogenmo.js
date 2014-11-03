15930f8fe6b5d03e630cb08864848461725f2529
Avner Shanan <ashanan@gmail.com>
2014-11-02 21:26:32 -0600

    Accept more than one word at a time as arguments.
    Each word creates a chapter.

--- nanogenmo.js
+++ nanogenmo.js
@@ -4,16 +4,27 @@ var liner = require('./liner');
 
 var source = process.argv[2];
 var word = process.argv[3];
-console.log(source);
-console.log(word);
+var words = process.argv.slice(3);
+
+var chapters = {};
+for(var i = 0; i < words.length; i++){
+	chapters[words[i]] = words[i] + ' in ' + source;
+}
 
 var sourceStream = fs.createReadStream(source);
 sourceStream.pipe(liner);
 liner.on('readable', function(){
 	var line;
 	while(line = liner.read()){
-		if(line.indexOf(word) > -1){
-			console.log(line);
+		for(var j = 0; j < words.length; j++){
+			word = words[j];
+			if(line.indexOf(word) > -1){
+				chapters[word] += line;
+			}
 		}
 	}
+});
+
+liner.on('finish', function(){
+	console.log(chapters);
 });
