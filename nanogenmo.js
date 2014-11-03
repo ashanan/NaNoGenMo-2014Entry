097c9775fab7732e198f7854598531dd93a333dc
Avner Shanan <ashanan@gmail.com>
2014-11-02 20:57:03 -0600

    Missed a file

--- nanogenmo.js
+++ nanogenmo.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
+var fs = require('fs');
+var liner = require('./liner');
 
-var source = process.argv[2]
-var word = process.argv[3]
+var source = process.argv[2];
+var word = process.argv[3];
 console.log(source);
 console.log(word);
+
+var sourceStream = fs.createReadStream(source);
+sourceStream.pipe(liner);
+liner.on('readable', function(){
+	var line;
+	while(line = liner.read()){
+		if(line.indexOf(word) > -1){
+			console.log(line);
+		}
+	}
+});
